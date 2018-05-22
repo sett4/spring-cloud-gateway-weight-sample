fb5ff279b623da28d48baeb4ef68ad0652cd2986
Setsu FUJITA <setsu.fujita@linecorp.com>
2018-05-22 13:17:21 +0900

    first commit for weight settings

---
+++ src/main/java/com/example/springcloudgatewayweight/SpringCloudGatewayWeightApplication.java
@@ -0,0 +1,12 @@
+package com.example.springcloudgatewayweight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayWeightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayWeightApplication.class, args);
+	}
+}
